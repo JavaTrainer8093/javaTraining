0857e1b66932e6f9e8d2ac7b9b3b911b8d7da082
Sambit <Sambit@DESKTOP-A8SH2NU>
2022-07-28 11:09:21 +0530

    provider

---
+++ SpringBootRestProviderApp/src/main/java/com/nt/SpringBootRestProviderAppApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestProviderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestProviderAppApplication.class, args);
+	}
+
+}
---
+++ SpringBootRestProviderApp/src/main/java/com/nt/controller/EmployeeRestController.java
@@ -0,0 +1,19 @@
+package com.nt.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeRestController {
+
+	
+	@GetMapping("/show")
+	public ResponseEntity<String> getMessage()
+	{
+		return ResponseEntity.ok("HI");
+	}
+	
+}
